e2226c0074f31b553bfc0e065e95f7158eb32f7f
safcsp dev <dev@test.com>
2024-02-10 23:27:53 +0400

    Add project files.

---
+++ NetworkInterfaceCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.NetworkInformation;
+
+
+NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+foreach (NetworkInterface inter in interfaces)
+{
+    Console.WriteLine();
+    Console.WriteLine("Id: "+ inter.Id);
+    Console.WriteLine("Interfaces: "+inter.Name);
+    Console.WriteLine("IsReceiveOnly: " + inter.IsReceiveOnly);
+    Console.WriteLine("Speed: " + inter.Speed);
+    Console.WriteLine("Description: " + inter.Description);
+    Console.WriteLine("Status: " + inter.OperationalStatus);
+    Console.WriteLine();
+
+
+
+
+}
